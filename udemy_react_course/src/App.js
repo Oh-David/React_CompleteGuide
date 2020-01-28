c39d26ea2a44e7d3ae4da82f8dad9a82725fa468
David Oh <david.oh@amerihome.com>
2020-01-28 13:02:37 -0800

    Functional vs Class Components

--- udemy_react_course/src/App.js
+++ udemy_react_course/src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsFunction ] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Dave', age: 29 },
       { name: 'Steph', age: 30 },
     ]
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Starting a React App</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  });
+
+  const [ otherState, setOtherState ] = useState({otherState: "other State values"});
+
+  console.log(personsState, otherState);
+
+  const buttonClickHandler = () => {
+    // console.log('Was clicked!');
+    setPersonsFunction({
+      persons: [
+        { name: 'A', age: 1 },
+        { name: 'B', age: 2 },
+        { name: 'C', age: 3 }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Starting a React App</h1>
+      <button onClick={buttonClickHandler}>Button Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
 export default App;
